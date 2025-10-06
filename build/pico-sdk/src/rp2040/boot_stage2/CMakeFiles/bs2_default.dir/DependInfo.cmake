069abdeffbc7442db517229ad85ef56ebb08c3e2
Ethan-Kotrba <kotrb011@umn.edu>
2025-10-06 17:21:52 -0500

    pior to making changes after getting debugger to work on blinking led

--- build/pico-sdk/src/rp2040/boot_stage2/CMakeFiles/bs2_default.dir/DependInfo.cmake
+++ build/pico-sdk/src/rp2040/boot_stage2/CMakeFiles/bs2_default.dir/DependInfo.cmake
@@ -8,13 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_ASM
-  "/home/acid/Clones/pico/pico-sdk/src/rp2040/boot_stage2/compile_time_choice.S" "/home/acid/Clones/pico/John-ADC-Git/build/pico-sdk/src/rp2040/boot_stage2/CMakeFiles/bs2_default.dir/compile_time_choice.S.o"
+  "/home/acid/Clones/pico/pico-sdk/src/rp2040/boot_stage2/compile_time_choice.S" "/home/acid/Clones/pico/John-ADC-Git/build/pico-sdk/src/rp2040/boot_stage2/CMakeFiles/bs2_default.dir/compile_time_choice.S.obj"
   )
 set(CMAKE_ASM_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_ASM
-  "LIB_BOOT_STAGE2_HEADERS=1"
   "PICO_32BIT=1"
   "PICO_BOARD=\"pico\""
   "PICO_BUILD=1"
@@ -32,7 +31,6 @@ set(CMAKE_ASM_TARGET_INCLUDE_PATH
   "generated/pico_base"
   "/home/acid/Clones/pico/pico-sdk/src/boards/include"
   "/home/acid/Clones/pico/pico-sdk/src/rp2040/pico_platform/include"
-  "/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_platform_common/include"
   "/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_platform_compiler/include"
   "/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_platform_panic/include"
   "/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_platform_sections/include"

58421b91b0116275b75b5ac98f64c76be75e89ca
Ethan-Kotrba <kotrb011@umn.edu>
2025-10-07 15:08:24 -0500

    init

--- PC_Side_Code/read.py
+++ PC_Side_Code/read.py
@@ -14,7 +14,7 @@ READ_TIMEOUT = 1      # Seconds
 # MCP3564 settings (match Pico code)
 VREF = 3.3           # Reference voltage (adjust if different in your setup)
 RESOLUTION = 32      # 24-bit ADC
-GAIN = 1             # Gain setting from CONFIG2
+# GAIN = 1             # Gain setting from CONFIG2
 
 def open_serial_port():
     try:

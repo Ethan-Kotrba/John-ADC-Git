f14b726b10027f66c7dc8dafd013b0b2b2c5af83
Ethan-Kotrba <kotrb011@umn.edu>
2025-10-20 10:27:14 -0500

    build files

--- build/CMakeFiles/John-ADC-Git.dir/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb.c.o.d
+++ build/CMakeFiles/John-ADC-Git.dir/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb.c.o.d
@@ -103,4 +103,5 @@ CMakeFiles/John-ADC-Git.dir/home/acid/Clones/pico/pico-sdk/src/rp2_common/pico_s
  /home/acid/Clones/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/m0plus.h \
  /home/acid/Clones/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/nvic.h \
  /home/acid/Clones/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/scb.h \
- /home/acid/Clones/pico/pico-sdk/lib/tinyusb/src/device/usbd_pvt.h
+ /home/acid/Clones/pico/pico-sdk/lib/tinyusb/src/device/usbd_pvt.h \
+ /home/acid/Clones/pico/pico-sdk/lib/tinyusb/src/common/tusb_private.h
